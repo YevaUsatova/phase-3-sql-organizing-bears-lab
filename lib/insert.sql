7cb85fbf3c4fef40f9425e36332633adaf1d4412
YevaUsatova <yeva.usatova@gmail.com>
2021-10-01 00:27:30 -0400

    commit tree b2636a743e1b11a97267d6e63d66074124301067

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "white", "cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "white-black", "awesome", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "brown", "crazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "white-brown", "amazing", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "green", "fuzzy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "F", "white", "agressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "white", NULL, 1);
